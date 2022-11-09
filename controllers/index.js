b825b4012156e211a1bc7486ff3b971550aa7dd9
Darx <mcrivabode@gmail.com>
2022-11-08 21:10:07 -0800

    set up controller files, set basis for router in index

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// set up router for modularity
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
